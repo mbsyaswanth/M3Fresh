0d85a3c718fa7be7069d4827cd3a6fdb7177449f
Srikanth <srikanthbhamidipalli.3@gmail.com>
2020-04-19 17:37:17 +0530

    added search bar in products page

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,26 +1,39 @@
 import React, { useContext } from "react";
 
-import { ProductsContainer } from "./styledComponents";
+import useCustomInputHandler from "../../customHooks/useInputHandler";
+import { StoreContext } from "../../App";
+
 import ItemCard from "../ItemCard";
 import NavBar from "../Navbar";
-import { StoreContext } from "../../App";
+
+import { ProductsContainer, StyledInput, Container } from "./styledComponents";
 
 function Products(props) {
   const [state, dispatch] = useContext(StoreContext);
-
+  const [values,handleChange] = useCustomInputHandler({filterText:''})
+  const getFilteredProducts=()=>{
+    return state.stocks.filter(stock => stock.product_name.includes(values.filterText))
+  }
+  const renderProductsList=()=>{
+    const filteredProducts = getFilteredProducts()
+    return filteredProducts.map((product) => (
+      <ItemCard
+        key={product.product_id}
+        name={product.product_name}
+        image={product.product_image}
+        price={product.price}
+        units={product.units}
+      />
+    ))
+  }
   return (
     <div>
       <NavBar />
+      <Container>
+      <StyledInput name={'filterText'} onChange={handleChange} value={values.filterText} placeholder={'Search...'}/>
+      </Container>
       <ProductsContainer>
-        {state.stocks.map((product) => (
-          <ItemCard
-            key={product.product_id}
-            name={product.product_name}
-            image={product.product_image}
-            price={product.price}
-            units={product.units}
-          />
-        ))}
+        {renderProductsList()}
       </ProductsContainer>
     </div>
   );
