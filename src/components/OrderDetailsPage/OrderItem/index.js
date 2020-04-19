51170746254c6837e35036e8bc675bf1984fc5f7
yaswanth <mbs.yaswanth@gmail.com>
2020-04-19 12:23:02 +0530

    added cart page and changed card styles

---
+++ src/components/OrderDetailsPage/OrderItem/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  Image,
+  Container,
+  ItemName,
+  Price,
+  QtyCount,
+  AddToCart,
+  DetailsContainer,
+  Details,
+  ItemPrice,
+  QuantityContainer,
+  QuantityButton,
+  Close,
+} from "./styledComponents";
+
+function ItemCard(props) {
+  const [count, setCount] = useState(props.count);
+  //TODO: add order count call back
+  return (
+    <Container>
+      <Close>x</Close>
+      <Image src={props.imageUrl} alt="item image" />
+      <DetailsContainer>
+        <Details>
+          <ItemName>{props.itemName}</ItemName>
+          <ItemPrice>
+            ₹ {props.price} / {props.unit}
+          </ItemPrice>
+          <QuantityContainer>
+            <QuantityButton onClick={() => setCount(count - 1)}>
+              -
+            </QuantityButton>
+            <QtyCount>{count}</QtyCount>
+            <QuantityButton onClick={() => setCount(count + 1)}>
+              +
+            </QuantityButton>
+          </QuantityContainer>
+        </Details>
+        <Price>₹ {props.price * count}</Price>
+      </DetailsContainer>
+    </Container>
+  );
+}
+
+export default ItemCard;
---
+++ src/components/OrderDetailsPage/styledComponents.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const NavBar = styled.div`
+  background: #d7e6d0;
+  padding: 10px 16px;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 37px;
+  line-height: 44px;
+  color: #178e1c;
+  text-align: left;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  z-index: 99;
+  box-shadow: 1px 0px 5px #178e1c;
+`;
+
+export const OrdersContainer = styled.div`
+  padding: 12px 16px;
+  margin: 64px 0px;
+`;
+
+export const OrdersHeading = styled.h3`
+  margin: 0;
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  text-align: left;
+  color: #178e1c;
+  margin-bottom: 10px;
+`;
+
+export const NextContainer = styled.div`
+  background: #d7e6d0;
+  position: fixed;
+  padding: 14px 16px;
+  bottom: 0px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 5px #178e1c;
+`;
+
+export const CartTotalHeading = styled.p`
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 21px;
+  line-height: 25px;
+  color: #178e1c;
+  margin: 0px;
+  display: inline-block;
+`;
+
+export const CartTotal = styled.p`
+  display: inline-block;
+  font-weight: normal;
+  color: #178e1c;
+  margin: 0px;
+`;
+
+export const NextButton = styled.button`
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 20px;
+  color: white;
+  background-color: #178e1c;
+  padding: 5px 15px;
+  border-radius: 23px;
+  border: none;
+  :hover {
+    background-color: #156e1b;
+  }
+`;
