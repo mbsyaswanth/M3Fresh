87beeaf03b61fabea4d53f957aecfd404ad73446
yaswanth <mbs.yaswanth@gmail.com>
2020-04-19 15:40:42 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { initFirebase } from "./firebase";
 import UserDetailForm from "./components/UserDetailForm";
-import ItemCard from "./components/ItemCard";
-import Products from "./components/Products";
 
 function App() {
   initFirebase();
 
-  return <Products />;
+  return <UserDetailForm />;
 }
 
 export default App;
