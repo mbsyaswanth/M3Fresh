46ec440560434ad8b92a02fca8435d3947776a96
yaswanth <mbs.yaswanth@gmail.com>
2020-04-19 19:25:04 +0530

    changed styles of card

--- src/components/Products/styledComponents.js
+++ src/components/Products/styledComponents.js
@@ -7,21 +7,24 @@ export const ProductsContainer = styled.div`
   grid-gap: 3%;
   grid-template-columns: repeat(auto-fit, minmax(160px, 180px));
   justify-content: center;
-  padding:10px;
-  padding-top:0px;
+  padding: 10px;
+  padding-top: 0px;
+  @media only screen and (max-width: 400px) {
+    grid-template-columns: repeat(auto-fit, minmax(130px, 160px));
+  }
 `;
 
 export const StyledInput = styled(Input)`
-width:300px;
-margin-top:10px;
-`
+  width: 300px;
+  margin-top: 10px;
+`;
 
-export const Container=styled.div`
-padding:10px;
-padding-top:85px;
-display:flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-box-sizing:border-box;
-`
+export const Container = styled.div`
+  padding: 10px;
+  padding-top: 85px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+`;
