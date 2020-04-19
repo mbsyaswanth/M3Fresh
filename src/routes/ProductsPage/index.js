f4f515d2e2444e177ba7f47e98b4ce4d4749c666
Srikanth <srikanthbhamidipalli.3@gmail.com>
2020-04-19 16:54:46 +0530

    solved merge conflicts

--- src/routes/ProductsPage/index.js
+++ src/routes/ProductsPage/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import { useHistory } from "react-router-dom";
 
@@ -7,6 +8,26 @@ import Products from "../../components/Products";
 const ProductsPage = () => {
   const history = useHistory();
   return <Products />;
+=======
+import React,{useState} from 'react';
+import {useHistory} from 'react-router-dom'
+
+import Input from '../../components/Input'
+
+const ProductsPage = () => {
+    const history = useHistory()
+    const [filtertext, setFilterText] = useState('')
+    const l=['srikanth','yashwanth','jagadish']
+    const showFilterItems=()=>{
+        return l.filter(i => i.includes(filtertext)) 
+    }
+    return (
+        <div>
+           <Input value={filtertext} onChange={(e)=> setFilterText(e.target.value)} placeholder={'Search...'}/>
+           {showFilterItems()}
+        </div>
+    );
+>>>>>>> Stashed changes
 };
 
 export default ProductsPage;
