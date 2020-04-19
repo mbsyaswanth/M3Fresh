b65c0ae46b91cd419c4f333647a06ef09c610e73
jagadish <jagadishreddykovvuri@gmail.com>
2020-04-19 20:28:24 +0530

    added cart icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,13 +3,24 @@ import { useHistory } from "react-router-dom";
 
 import { goToHomePage } from "../../utils/RouteUtils";
 
-import { StyledNavBarContainer } from "./styledComponents";
+import {
+  StyledNavBarContainer,
+  CartIconContainer,
+  Badge,
+} from "./styledComponents";
+import CartIcon from "./CartIcon";
 
 function Navbar(props) {
   const history = useHistory();
   return (
     <StyledNavBarContainer>
       <span onClick={() => goToHomePage(history)}>M3Fresh.</span>
+      {props.showCart && props.count > 0 ? (
+        <CartIconContainer>
+          <Badge>{props.count}</Badge>
+          <CartIcon />
+        </CartIconContainer>
+      ) : null}
     </StyledNavBarContainer>
   );
 }
